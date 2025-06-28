52cd52e2e616482f368a834cdb7645a6e6171af6
togetherwehike <togetherwehike.cm@gmail.com>
2025-06-28 09:18:15 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,1 @@
+// App.jsx will be filled next
---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<HomePage />);
